72f9848dc3d865e6aaa3c5e77182481462447260
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-24 19:07:23 -0800

    Introduce initial registration of projects taxonomy

--- cranberry.php
+++ cranberry.php
@@ -16,10 +16,11 @@ if ( ! defined( 'WPINC' ) ) {
 // The core plugin class.
 require dirname( __FILE__ ) . '/includes/class-cranberry.php';
 require dirname( __FILE__ ) . '/includes/class-cranberry-task.php';
+require dirname( __FILE__ ) . '/includes/class-cranberry-project.php';
 
 add_action( 'after_setup_theme', 'Cranberry' );
 /**
- * Start things up.
+ * Initializes the main Cranberry plugin controller.
  *
  * @return \Cranberry
  */
@@ -29,7 +30,7 @@ function Cranberry() {
 
 add_action( 'after_setup_theme', 'Cranberry_Task' );
 /**
- * Initialize the handling of tasks.
+ * Initializes the handling of tasks.
  *
  * @since 0.0.1
  *
@@ -37,4 +38,16 @@ add_action( 'after_setup_theme', 'Cranberry_Task' );
  */
 function Cranberry_Task() {
 	return Cranberry_Task::get_instance();
+}
+
+add_action( 'after_setup_theme', 'Cranberry_Project' );
+/**
+ * Initializes the handling of projects.
+ *
+ * @since 0.0.1
+ *
+ * @return Cranberry_Project
+ */
+function Cranberry_Project() {
+	return Cranberry_Project::get_instance();
 }
