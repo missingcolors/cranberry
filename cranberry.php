42389fdbda60e7c34e17bdf86d4e5a283212e3f9
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-24 19:10:01 -0800

    Allow the main controller to register individual modules

--- cranberry.php
+++ cranberry.php
@@ -27,27 +27,3 @@ add_action( 'after_setup_theme', 'Cranberry' );
 function Cranberry() {
 	return Cranberry::get_instance();
 }
-
-add_action( 'after_setup_theme', 'Cranberry_Task' );
-/**
- * Initializes the handling of tasks.
- *
- * @since 0.0.1
- *
- * @return Cranberry_Task
- */
-function Cranberry_Task() {
-	return Cranberry_Task::get_instance();
-}
-
-add_action( 'after_setup_theme', 'Cranberry_Project' );
-/**
- * Initializes the handling of projects.
- *
- * @since 0.0.1
- *
- * @return Cranberry_Project
- */
-function Cranberry_Project() {
-	return Cranberry_Project::get_instance();
-}
